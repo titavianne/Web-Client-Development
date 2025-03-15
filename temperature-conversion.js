84b5839c49fb0ef6a02520c982b36cd9faca56bf
Tita Noviana <titavianne@gmail.com>
2025-03-16 03:46:44 +0700

    task 1 temperature conversion

---
+++ temperature-conversion.js
@@ -0,0 +1,5 @@
+// 1. Konversi Fahrenheit ke Celsius
+function fahrenheitToCelsius(fahrenheit: number): number {
+  return (fahrenheit - 32) * 5 / 9;
+}
+console.log(fahrenheitToCelsius(100)); // Contoh output: 37.7778
